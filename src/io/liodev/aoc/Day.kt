a738de549a03c43f4762af404b87399f14cde2fb
LH23 <lionel.hubmann@deptagency.com>
2023-11-29 17:33:17 -0300

    added template and changed printed message

--- src/io/liodev/aoc/Day.kt
+++ src/io/liodev/aoc/Day.kt
@@ -10,13 +10,13 @@ fun runDay(dayTest: Day<*>, dayReal: Day<*>) {
     println("\n")
 
     val resultTest1 = dayTest.solvePart1()
-    checkResult("Test 1", resultTest1, dayTest.expectedValues[0])
+    checkResult("Test Part1", resultTest1, dayTest.expectedValues[0])
     val result1 = dayReal.solvePart1()
-    checkResult("Real 1", result1, dayReal.expectedValues[1])
+    checkResult("Real Part1", result1, dayReal.expectedValues[1])
 
     val resultTest2 = dayTest.solvePart2()
-    checkResult("Test 2", resultTest2, dayTest.expectedValues[2])
+    checkResult("Test Part2", resultTest2, dayTest.expectedValues[2])
     val result2 = dayReal.solvePart2()
-    checkResult("Real 2", result2, dayReal.expectedValues[3])
+    checkResult("Real Part2", result2, dayReal.expectedValues[3])
 
 }
