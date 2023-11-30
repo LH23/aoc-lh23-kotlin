f0765066ee2797e4a73d4d41d5e544e411aaa15e
LH23 <lionel.hubmann@deptagency.com>
2023-11-30 18:50:50 -0300

    improvements on Day05 readability

--- src/io/liodev/aoc/aoc2022/Day05.kt
+++ src/io/liodev/aoc/aoc2022/Day05.kt
@@ -8,50 +8,48 @@ import java.util.Stack
 class Day05(private val input: String) : Day<String> {
     override val expectedValues = listOf("CMZ", "CWMTGHBDW", "MCD", "SSCGWJCRB")
 
-    private val instructions = parseInstuctions(input)
-    private fun parseInstuctions(input: String) = input
+    private val instructions = parseInstructions(input)
+    private fun parseInstructions(input: String) = input
         .substring(input.indexOf("move "))
-        .split('\n').map { inst ->
-            val regex = Regex("""move (\d+) from (\d+) to (\d+)""")
-            val (i, f, t) = regex.find(inst)!!.destructured
-            Instruction(i.toInt(), f.toInt(), t.toInt())
-        }
+        .split('\n')
+        .map { it.toInstruction() }
 
-    private fun parseStacks(input: String) = ShipStacks(
-        input
+    private fun parseStacks(input: String) = input
             .substring(0, input.indexOf("\n 1 "))
             .split('\n')
             .map { it.toRow() }
             .createStacks()
-    )
 
-    class ShipStacks(val stacks: List<Stack<Char>>) {
-        fun moveWithCrateMover9000(inst: Instruction) {
-            repeat(inst.num) {
-                stacks[inst.to - 1].push(stacks[inst.from - 1].pop())
-            }
-        }
-
-        fun moveWithCrateMover9001(inst: Instruction) {
-            val crates = stacks[inst.from - 1].takeLast(inst.num)
-            stacks[inst.to - 1].addAll(crates)
-            repeat(inst.num) { stacks[inst.from - 1].removeLast() }
+    private fun moveWithCrateMover9000(inst: Instruction, stacks: List<Stack<Char>>) {
+        repeat(inst.num) {
+            stacks[inst.to - 1].push(stacks[inst.from - 1].pop())
         }
     }
 
-    class Instruction(val num: Int, val from: Int, val to: Int)
+    private fun moveWithCrateMover9001(inst: Instruction, stacks: List<Stack<Char>>) {
+        val crates = stacks[inst.from - 1].takeLast(inst.num)
+        stacks[inst.to - 1].addAll(crates)
+        repeat(inst.num) { stacks[inst.from - 1].removeLast() }
+    }
 
     override fun solvePart1(): String {
         val stacks = parseStacks(input)
-        instructions.forEach { stacks.moveWithCrateMover9000(it) }
-        return stacks.stacks.map { it.pop() }.joinToString("")
+        instructions.forEach { moveWithCrateMover9000(it,stacks) }
+        return stacks.map { it.pop() }.joinToString("")
     }
 
     override fun solvePart2(): String {
         val stacks = parseStacks(input)
-        instructions.forEach { stacks.moveWithCrateMover9001(it) }
-        return stacks.stacks.map { it.pop() }.joinToString("")
+        instructions.forEach { moveWithCrateMover9001(it, stacks) }
+        return stacks.map { it.pop() }.joinToString("")
     }
+}
+
+class Instruction(val num: Int, val from: Int, val to: Int)
+private fun String.toInstruction(): Instruction {
+    val regex = Regex("""move (\d+) from (\d+) to (\d+)""")
+    val (i, f, t) = regex.find(this)!!.destructured
+    return Instruction(i.toInt(), f.toInt(), t.toInt())
 }
 
 private fun List<List<Char>>.createStacks(): List<Stack<Char>> {
@@ -67,10 +65,9 @@ private fun List<List<Char>>.createStacks(): List<Stack<Char>> {
 private fun String.toRow(): List<Char> {
     return this
         .replace("    ", "[-]")
-        .replace("] [", "")
-        .replace("]", "")
-        .replace("[", "")
-        .map { it }
+        .replace("] [", "][")
+        .chunked(3)
+        .map { it[1] }
 }
 
 
