c7e26b98d24872d6a7d870897b91ae365ebe4e0a
LH23 <lionel.hubmann@deptagency.com>
2023-12-06 03:05:11 -0300

    added a skip option to runDay

--- src/io/liodev/aoc/Day.kt
+++ src/io/liodev/aoc/Day.kt
@@ -8,27 +8,39 @@ interface Day<T> {
     fun solvePart2(): T
 }
 
-fun runDay(dayTest: Day<*>, dayReal: Day<*>, printTimings: Boolean = false) {
+fun runDay(
+    dayTest: Day<*>,
+    dayReal: Day<*>,
+    printTimings: Boolean = false,
+    skipTests: List<Boolean> = listOf(false, false, false, false)
+) {
     val christmasSeparator = "❆⋆꙳•✩⋆☃⋆✩•꙳⋆❆"
     println("\n$christmasSeparator⋆❆⋆꙳ ${dayTest.javaClass.simpleName} ꙳⋆❆⋆${christmasSeparator}")
 
     val elapsedPart1 = measureTime {
-        val resultTest1 = dayTest.solvePart1()
-        checkResult("Test Part1", resultTest1, dayTest.expectedValues[0])
-        val result1 = dayReal.solvePart1()
-        checkResult("Real Part1", result1, dayReal.expectedValues[1])
+        if (!skipTests[0]) {
+            val resultTest1 = dayTest.solvePart1()
+            checkResult("Test Part1", resultTest1, dayTest.expectedValues[0])
+        }
+        if (!skipTests[1]) {
+            val result1 = dayReal.solvePart1()
+            checkResult("Real Part1", result1, dayReal.expectedValues[1])
+        }
     }
     if (printTimings) println(" Elapsed Part1: $elapsedPart1")
     println("$christmasSeparator⋆$christmasSeparator⋆$christmasSeparator")
 
     val elapsedPart2 = measureTime {
-        val resultTest2 = dayTest.solvePart2()
-        checkResult("Test Part2", resultTest2, dayTest.expectedValues[2])
-        val result2 = dayReal.solvePart2()
-        checkResult("Real Part2", result2, dayReal.expectedValues[3])
+        if (!skipTests[2]) {
+            val resultTest2 = dayTest.solvePart2()
+            checkResult("Test Part2", resultTest2, dayTest.expectedValues[2])
+        }
+        if (!skipTests[3]) {
+            val result2 = dayReal.solvePart2()
+            checkResult("Real Part2", result2, dayReal.expectedValues[3])
+        }
     }
     if (printTimings) println(" Elapsed Part2: $elapsedPart2")
     println("$christmasSeparator⋆$christmasSeparator⋆$christmasSeparator")
-
 
 }
