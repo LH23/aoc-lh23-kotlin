2b1da5e9541c23994861d13fc2d1db15d8e57b95
LH23 <lionel.hubmann@deptagency.com>
2023-12-09 02:30:20 -0300

    Day09, first version

---
+++ src/io/liodev/aoc/aoc2023/Day09.kt
@@ -0,0 +1,40 @@
+package io.liodev.aoc.aoc2023
+
+import io.liodev.aoc.Day
+import io.liodev.aoc.readInputAsString
+import io.liodev.aoc.runDay
+
+// 2023 Day09
+class Day09(input: String) : Day<Long> {
+    override val expectedValues = listOf(114L, 1637452029, 2, 908)
+
+    private val histories = input.split("\n").map { it.split(" ").map { it.toLong() } }
+
+    override fun solvePart1(): Long =
+        histories.sumOf { history -> history.calculateNextElement() }
+
+    private fun List<Long>.calculateNextElement(): Long {
+        val seq = this.zipWithNext { a, b -> b - a }
+
+        return if (seq.all { it == 0L }) last()
+        else last() + seq.calculateNextElement()
+    }
+
+    override fun solvePart2() =
+        histories.sumOf { history -> history.calculatePreviousElement() }
+
+    private fun List<Long>.calculatePreviousElement(): Long {
+        val seq = this.zipWithNext { a, b -> b - a }
+
+        return if (seq.all { it == 0L }) first()
+        else first() - seq.calculatePreviousElement()
+    }
+}
+
+
+fun main() {
+    val name = Day09::class.simpleName
+    val testInput = readInputAsString("src/input/2023/${name}_test.txt")
+    val realInput = readInputAsString("src/input/2023/${name}.txt")
+    runDay(Day09(testInput), Day09(realInput))
+}
