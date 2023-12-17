258c2c3544aab6d1a33b0bc7b7d5c5782bd94407
LH23 <lionel.hubmann@deptagency.com>
2023-12-17 20:53:22 -0300

    Day17, fix in part2 after refactor, some extra cleanup

--- src/io/liodev/aoc/aoc2023/Day17.kt
+++ src/io/liodev/aoc/aoc2023/Day17.kt
@@ -1,16 +1,13 @@
 package io.liodev.aoc.aoc2023
 
 import io.liodev.aoc.Day
+import io.liodev.aoc.println
 import io.liodev.aoc.readInputAsString
 import io.liodev.aoc.runDay
 import io.liodev.aoc.utils.Coord
-import io.liodev.aoc.utils.set
 import io.liodev.aoc.utils.get
-import io.liodev.aoc.utils.printMatrix
-import io.liodev.aoc.utils.printPathInMatrix
 import io.liodev.aoc.utils.validIndex
 import java.util.PriorityQueue
-import kotlin.math.abs
 
 // --- 2023 Day 17: Clumsy Crucible ---
 class Day17(input: String) : Day<Int> {
@@ -21,28 +18,28 @@ class Day17(input: String) : Day<Int> {
     override fun solvePart1() = findOptimalPath(
         Coord(0, 0),
         Coord(heatLossMap.lastIndex, heatLossMap[0].lastIndex),
-        checkDirCondition = { it.currentDir == it.parent?.currentDir && it.stepsInDir > 3 }
+        excludeDirectionIf = { it.currentDir == it.parent?.currentDir && it.stepsInDir > 3 }
     ).sumOf { heatLossMap[it] }
 
     override fun solvePart2() = findOptimalPath(
         Coord(0, 0),
         Coord(heatLossMap.lastIndex, heatLossMap[0].lastIndex),
-        checkDirCondition = {
-            (it.currentDir == it.parent?.currentDir && it.stepsInDir == 10) ||
-                    (it.currentDir != it.parent?.currentDir && it.stepsInDir >= 4)
+        excludeDirectionIf = {
+            (it.currentDir == it.parent?.currentDir && it.stepsInDir > 10) ||
+            (it.currentDir != it.parent?.currentDir && it.parent?.stepsInDir!! < 4)
         }
     ).sumOf { heatLossMap[it] }
 
     private fun findOptimalPath(
         origin: Coord,
         end: Coord,
-        checkDirCondition: (Node) -> Boolean
+        excludeDirectionIf: (Node) -> Boolean
     ): List<Coord> {
         val openPri = PriorityQueue<Node>() { a, b -> a.f - b.f }
         val openMap = HashMap<Node, Int>()
         val closedSet = HashSet<Node>()
 
-        val nodeStart = Node(origin, 0, 0, null)
+        val nodeStart = Node(origin, 1, 0, null)
         openPri.offer(nodeStart)
 
         while (openPri.isNotEmpty()) {
@@ -50,43 +47,36 @@ class Day17(input: String) : Day<Int> {
             closedSet.add(current)
 
             if (current.position == end) {
-                val path = mutableListOf<Coord>()
-                var next = current
-                while (next.parent != null) {
-                    path.add(next.position)
-                    next = next.parent!!
-                }
-                //heatLossMap.printPathInMatrix(path, 0)
-                return path.reversed()
+                return constructPath(current)
             }
 
             for ((i, n) in current.position.getCardinalBorder().withIndex()) {
                 if (!heatLossMap.validIndex(n) || n == current.parent?.position) continue
 
-                val node =
-                    Node(n, i, if (current.currentDir == i) current.stepsInDir + 1 else 1, current)
+                val node = Node(n, i, if (current.currentDir == i) current.stepsInDir + 1 else 1, current)
                 node.g = current.g + heatLossMap[node.position]
-                node.h =
-                    abs(node.position.r - end.r) + abs(node.position.c - end.c) // manhattan distance
+                node.h = 0 //abs(node.position.r - end.r) + abs(node.position.c - end.c)
                 node.f = node.g + node.h
 
                 if (closedSet.contains(node)) continue
-
-                if (checkDirCondition(node)) continue
-
-                val cp = openMap[node]
-                //println("CP of $node = $cp <= ${node.g}?")
-                if (cp != null && cp <= node.g) {
-                    //println("Discarded $n by saved g: $cp <= ${node.g}")
-                    continue
-                }
+                if (openMap.getOrElse(node) {Int.MAX_VALUE} <= node.g) continue
+                if (excludeDirectionIf(node)) continue
 
                 openPri.offer(node)
-                //println("Added $node, size ${openPri.size}")
                 openMap[node] = node.g
             }
         }
         return listOf()
+    }
+
+    private fun constructPath(node: Node): List<Coord> {
+        val path = mutableListOf<Coord>()
+        var next = node
+        while (next.parent != null) {
+            path.add(next.position)
+            next = next.parent!!
+        }
+        return path.reversed()
     }
 }
 
@@ -95,8 +85,7 @@ data class Node(val position: Coord, val currentDir: Int, val stepsInDir: Int, v
     var g = 0
     var h = 0
     override fun toString() =
-        "(${position} $stepsInDir $currentDir)"
-
+        "(${position} $currentDir $stepsInDir)"
     override fun equals(other: Any?): Boolean = other is Node && this.toString() == other.toString()
     override fun hashCode(): Int = this.toString().hashCode()
 }
@@ -109,7 +98,7 @@ fun main() {
     runDay(
         Day17(testInput),
         Day17(realInput),
-        extraDays = listOf(Day17(testInput2)),
+        //extraDays = listOf(Day17(testInput2)),
         printTimings = true
     )
 }
