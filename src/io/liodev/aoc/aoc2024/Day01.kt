72b26b9598113d50e81b1c9d4d4e3be200735112
LH23 <lionelhubmann@gmail.com>
2024-12-01 02:31:02 -0300

    Day1 cleanup

--- src/io/liodev/aoc/aoc2024/Day01.kt
+++ src/io/liodev/aoc/aoc2024/Day01.kt
@@ -11,19 +11,19 @@ class Day01(
 ) : Day<Int> {
     override val expectedValues = listOf(11, 1189304, 31, 24349736)
 
-    private val listPairs =
+    private val listsPair =
         input
             .split("\n")
-            .map { line -> line.split("   ") }
-            .let { it.map { it[0].toInt() } to it.map { it[1].toInt() } }
+            .map { line -> line.split("   ").map { it.toInt() } }
+            .let { nums -> nums.map { it[0] } to nums.map { it[1] } }
 
     override fun solvePart1() =
-        listPairs.let { (firstList, secondList) ->
+        listsPair.let { (firstList, secondList) ->
             firstList.sorted().zip(secondList.sorted()).sumOf { abs(it.first - it.second) }
         }
 
     override fun solvePart2() =
-        listPairs.let { (firstList, secondList) ->
+        listsPair.let { (firstList, secondList) ->
             firstList.sumOf { a -> a * secondList.count { b -> b == a } }
         }
 }
