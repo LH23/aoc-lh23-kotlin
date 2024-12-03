a5ce2af7593b7c38e15ac273a2aa295a8291a4bc
LH23 <lionelhubmann@gmail.com>
2024-12-03 15:13:22 -0300

    Day02 O(n) version part2

--- src/io/liodev/aoc/aoc2024/Day02.kt
+++ src/io/liodev/aoc/aoc2024/Day02.kt
@@ -14,7 +14,7 @@ class Day02(
 
     override fun solvePart1() = reportLevels.count { safe(it) }
 
-    override fun solvePart2() = reportLevels.count { safe(it) || toleratingOneError(it) }
+    override fun solvePart2() = reportLevels.count { safe(it) || toleratingOneError2(it) }
 
     private fun safe(levels: List<Int>): Boolean =
         levels.zipWithNext().let { zips ->
@@ -22,11 +22,31 @@ class Day02(
                 zips.all { (a, b) -> a > b && a - b < 4 }
         }
 
+    // Quadratic version
     private fun toleratingOneError(levels: List<Int>): Boolean =
         levels.indices.any { i ->
             safe(levels.subList(0, i) + levels.subList(i + 1, levels.size))
         }
 
+    // O(N) version
+    private fun toleratingOneError2(levels: List<Int>): Boolean {
+        val index = indexOfFirstFail(levels)
+        return (
+            safe(levels.filterIndexed { i, _ -> i != index }) ||
+                safe(levels.filterIndexed { i, _ -> i != index - 1 })
+        )
+    }
+
+    private fun indexOfFirstFail(levels: List<Int>): Int {
+        val zips = levels.zipWithNext()
+        val ascendingCount = zips.count { (a, b) -> a < b }
+
+        return if (ascendingCount > levels.size / 2) {
+            zips.indexOfFirst { (a, b) -> !(a < b && b - a < 4) } + 1
+        } else {
+            zips.indexOfFirst { (a, b) -> !(a > b && a - b < 4) } + 1
+        }
+    }
 }
 
 fun main() {
@@ -34,5 +54,5 @@ fun main() {
     val year = 2024
     val testInput = readInputAsString("src/input/$year/${name}_test.txt")
     val realInput = readInputAsString("src/input/$year/$name.txt")
-    runDay(Day02(testInput), Day02(realInput), year)
+    runDay(Day02(testInput), Day02(realInput), year, printTimings = true)
 }
