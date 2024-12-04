a8f6fea6f3f16075e2f92ce57473611990f486ae
LH23 <lionelhubmann@gmail.com>
2024-12-04 03:06:15 -0300

    Day04 some cleanup

--- src/io/liodev/aoc/aoc2024/Day04.kt
+++ src/io/liodev/aoc/aoc2024/Day04.kt
@@ -4,8 +4,8 @@ import io.liodev.aoc.Day
 import io.liodev.aoc.readInputAsString
 import io.liodev.aoc.runDay
 import io.liodev.aoc.utils.Coord
-import io.liodev.aoc.utils.validIndex
 import io.liodev.aoc.utils.get
+import io.liodev.aoc.utils.validIndex
 
 // --- 2024 Day 4: Ceres Search ---
 class Day04(
@@ -19,15 +19,18 @@ class Day04(
         var count = 0
         for (i in wordSearch.indices) {
             for (j in wordSearch[0].indices) {
-                if (wordSearch.isXmas(Coord(i,j), Coord(i + 1,j + 0), Coord(i + 2,j + 0), Coord(i + 3,j + 0))) count++
-                if (wordSearch.isXmas(Coord(i,j), Coord(i - 1,j + 0), Coord(i - 2,j + 0), Coord(i - 3,j + 0))) count++
-                if (wordSearch.isXmas(Coord(i,j), Coord(i + 0,j + 1), Coord(i + 0,j + 2), Coord(i + 0,j + 3))) count++
-                if (wordSearch.isXmas(Coord(i,j), Coord(i + 0,j - 1), Coord(i + 0,j - 2), Coord(i + 0,j - 3))) count++
+                val ij = Coord(i, j)
+                if (wordSearch[ij] == 'X') {
+                    if (wordSearch.isXmas(ij, ij.goDown(1), ij.goDown(2), ij.goDown(3))) count++
+                    if (wordSearch.isXmas(ij, ij.goUp(1), ij.goUp(2), ij.goUp(3))) count++
+                    if (wordSearch.isXmas(ij, ij.goLeft(1), ij.goLeft(2), ij.goLeft(3))) count++
+                    if (wordSearch.isXmas(ij, ij.goRight(1), ij.goRight(2), ij.goRight(3))) count++
 
-                if (wordSearch.isXmas(Coord(i,j), Coord(i + 1,j + 1), Coord(i + 2,j + 2), Coord(i + 3,j + 3))) count++
-                if (wordSearch.isXmas(Coord(i,j), Coord(i + 1,j - 1), Coord(i + 2,j - 2), Coord(i + 3,j - 3))) count++
-                if (wordSearch.isXmas(Coord(i,j), Coord(i - 1,j + 1), Coord(i - 2,j + 2), Coord(i - 3,j + 3))) count++
-                if (wordSearch.isXmas(Coord(i,j), Coord(i - 1,j - 1), Coord(i - 2,j - 2), Coord(i - 3,j - 3))) count++
+                    if (wordSearch.isXmas(ij, ij.goDown(1).goRight(1), ij.goDown(2).goRight(2), ij.goDown(3).goRight(3))) count++
+                    if (wordSearch.isXmas(ij, ij.goDown(1).goLeft(1), ij.goDown(2).goLeft(2), ij.goDown(3).goLeft(3))) count++
+                    if (wordSearch.isXmas(ij, ij.goUp(1).goRight(1), ij.goUp(2).goRight(2), ij.goUp(3).goRight(3))) count++
+                    if (wordSearch.isXmas(ij, ij.goUp(1).goLeft(1), ij.goUp(2).goLeft(2), ij.goUp(3).goLeft(3))) count++
+                }
             }
         }
         return count
@@ -35,13 +38,14 @@ class Day04(
 
     override fun solvePart2(): Int {
         var count = 0
-        for (i in wordSearch.indices) {
-            for (j in wordSearch[0].indices) {
-                if (wordSearch.isXCrossedMas(Coord(i - 1 , j - 1), Coord(i,j), Coord(i + 1, j + 1), Coord(i - 1, j + 1), Coord(i + 1, j - 1))) count++
-                if (wordSearch.isXCrossedMas(Coord(i + 1 , j + 1), Coord(i,j), Coord(i - 1, j - 1), Coord(i + 1, j - 1), Coord(i - 1, j + 1))) count++
-
-                if (wordSearch.isXCrossedMas(Coord(i - 1 , j - 1), Coord(i,j), Coord(i + 1, j + 1), Coord(i + 1, j - 1), Coord(i - 1, j + 1))) count++
-                if (wordSearch.isXCrossedMas(Coord(i + 1 , j + 1), Coord(i,j), Coord(i - 1, j - 1), Coord(i - 1, j + 1), Coord(i + 1, j - 1))) count++
+        for (i in 1..<wordSearch.lastIndex) {
+            for (j in 1..<wordSearch[0].lastIndex) {
+                val ij = Coord(i, j)
+                if (wordSearch[ij] == 'A') {
+                    if (wordSearch.isXCrossedMasPattern(i, j)) {
+                        count++
+                    }
+                }
             }
         }
         return count
@@ -51,19 +55,62 @@ class Day04(
         x: Coord,
         m: Coord,
         a: Coord,
-        s: Coord
-    ): Boolean = x.validIndex(this) && m.validIndex(this) && a.validIndex(this) && s.validIndex(this) &&
-            this[x] == 'X' && this[m] == 'M' && this[a] == 'A' && this[s] == 'S'
+        s: Coord,
+    ): Boolean =
+        x.validIndex(this) &&
+            m.validIndex(this) &&
+            a.validIndex(this) &&
+            s.validIndex(this) &&
+            this[x] == 'X' &&
+            this[m] == 'M' &&
+            this[a] == 'A' &&
+            this[s] == 'S'
+
+    private fun List<List<Char>>.isXCrossedMasPattern(
+        i: Int,
+        j: Int,
+    ) = this.isXCrossedMas(
+        Coord(i - 1, j - 1),
+        Coord(i, j),
+        Coord(i + 1, j + 1),
+        Coord(i - 1, j + 1),
+        Coord(i + 1, j - 1),
+    ) ||
+        this.isXCrossedMas(
+            Coord(i + 1, j + 1),
+            Coord(i, j),
+            Coord(i - 1, j - 1),
+            Coord(i + 1, j - 1),
+            Coord(i - 1, j + 1),
+        ) ||
+        this.isXCrossedMas(
+            Coord(i - 1, j - 1),
+            Coord(i, j),
+            Coord(i + 1, j + 1),
+            Coord(i + 1, j - 1),
+            Coord(i - 1, j + 1),
+        ) ||
+        this.isXCrossedMas(
+            Coord(i + 1, j + 1),
+            Coord(i, j),
+            Coord(i - 1, j - 1),
+            Coord(i - 1, j + 1),
+            Coord(i + 1, j - 1),
+        )
 
     private fun List<List<Char>>.isXCrossedMas(
         m: Coord,
         a: Coord,
         s: Coord,
         m2: Coord,
-        s2: Coord
-    ): Boolean = (m.validIndex(this) && a.validIndex(this) && s.validIndex(this) && m2.validIndex(this) && s2.validIndex(this) &&
-            this[m] == 'M' && this[a] == 'A' && this[s] == 'S' && this[m2] == 'M' && this[s2] == 'S')
-
+        s2: Coord,
+    ): Boolean =
+        listOf(m, a, s, m2, s2).all { it.validIndex(this) } &&
+            this[m] == 'M' &&
+            this[a] == 'A' &&
+            this[s] == 'S' &&
+            this[m2] == 'M' &&
+            this[s2] == 'S'
 }
 
 fun main() {
@@ -71,5 +118,8 @@ fun main() {
     val year = 2024
     val testInput = readInputAsString("src/input/$year/${name}_test.txt")
     val realInput = readInputAsString("src/input/$year/$name.txt")
-    runDay(Day04(testInput), Day04(realInput), year)
+    runDay(Day04(testInput), Day04(realInput), year, printTimings = true)
+    // INITIAL TIMES
+    // Elapsed Part1: 26.338042ms
+    // Elapsed Part2: 15.005125ms
 }
