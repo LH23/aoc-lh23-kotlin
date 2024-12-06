2343f55adef18ed01e76ba92a649a91d2c4da7ac
LH23 <lionelhubmann@gmail.com>
2024-12-06 10:33:26 -0300

    Day06 add alreadyVisited for speedup

--- src/io/liodev/aoc/aoc2024/Day06.kt
+++ src/io/liodev/aoc/aoc2024/Day06.kt
@@ -21,6 +21,8 @@ class Day06(
             .toSet()
             .size
 
+    override fun solvePart2(): Int = calculateCycles(calculateGuardPath(guardPosition(walkingMap), Dir.North)).size
+
     private fun guardPosition(walkingMap: List<List<Char>>): Coord {
         for (r in walkingMap.indices) {
             for (c in walkingMap[r].indices) {
@@ -37,45 +39,45 @@ class Day06(
         initialDir: Dir,
         visitedAlready: Set<Pair<Coord, Dir>> = setOf(),
         newObstacle: Coord? = null,
-    ): List<Pair<Coord, Dir>> {
+    ): Set<Pair<Coord, Dir>> {
         var currentPos = guardPosition.copy()
         var currentDir = initialDir
         val visited = mutableSetOf(currentPos to currentDir)
-        visited.addAll(visitedAlready)
         while (true) {
             if (walkingMap[currentPos.move(currentDir)] == '#' || currentPos.move(currentDir) == newObstacle) {
                 currentDir = currentDir.turnRight()
             } else {
                 currentPos = currentPos.move(currentDir)
-                if (currentPos to currentDir in visited) {
+                val current = currentPos to currentDir
+                if (current in visited || current in visitedAlready) {
                     visited += Coord(-1, -1) to Dir.North
                     break
                 }
-                visited += currentPos to currentDir
+                visited += current
                 if (!currentPos.move(currentDir).validIndex(walkingMap)) break
             }
         }
-        return visited.toList()
+        return visited
     }
 
-    override fun solvePart2(): Int {
-        val guardPath = calculateGuardPath(guardPosition(walkingMap), Dir.North)
+    private fun calculateCycles(guardPath: Set<Pair<Coord, Dir>>): Set<Coord> {
         val visitedAlready = mutableSetOf<Pair<Coord, Dir>>()
         val obstacles = mutableSetOf<Coord>()
-        val guard = guardPosition(walkingMap)
+
         for (position in guardPath.drop(1)) {
             val newObstacle = position.first
-            if (newObstacle in obstacles || newObstacle in visitedAlready.map { it.first }) {
+            if (newObstacle in visitedAlready.map { it.first }) {
+                // already tested
                 continue
             }
-            // TODO use visitedAlready to speed up
-            val theoreticalPath = calculateGuardPath(guard, Dir.North, setOf(), newObstacle)
+            val (lastPos, lastDir) = visitedAlready.lastOrNull() ?: guardPath.first()
+            val theoreticalPath = calculateGuardPath(lastPos, lastDir, visitedAlready, newObstacle)
             if (theoreticalPath.last().first == Coord(-1, -1)) {
                 obstacles += newObstacle
             }
             visitedAlready += position
         }
-        return obstacles.size
+        return obstacles
     }
 }
 
