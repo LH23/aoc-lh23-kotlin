80a82d64f6aefb73fc85043e009e551690e818d8
LH23 <lionelhubmann@gmail.com>
2024-12-11 03:48:33 -0300

    Day11 cleanup but no speedup :(

--- src/io/liodev/aoc/aoc2024/Day11.kt
+++ src/io/liodev/aoc/aoc2024/Day11.kt
@@ -3,8 +3,9 @@ package io.liodev.aoc.aoc2024
 import io.liodev.aoc.Day
 import io.liodev.aoc.readInputAsString
 import io.liodev.aoc.runDay
+import kotlin.math.pow
 
-// --- 2024 11
+// --- 2024 Day 11: Plutonian Pebbles ---
 class Day11(
     input: String,
 ) : Day<Long> {
@@ -12,39 +13,37 @@ class Day11(
 
     private val initialArrangement = input.split(" ").map { it.toLong() }
 
-    override fun solvePart1(): Long {
-        var totalStones = 0L
-        for (stone in initialArrangement) {
-            totalStones += calculate25Blinks(stone, mutableMapOf()).size
-        }
-        println(totalStones)
-        return totalStones
-    }
+    override fun solvePart1(): Long =
+        initialArrangement
+            .sumOf { stone -> calculate25Blinks(stone).size }
+            .toLong()
 
     override fun solvePart2(): Long {
-        val stonesMap = mutableMapOf<Long, List<Long>>()
+        val stonesMap25 = mutableMapOf<Long, List<Long>>()
         var totalStones = 0L
 
         for (stone in initialArrangement) {
-            val stones = calculate25Blinks(stone, stonesMap)
-            stonesMap[stone] = stones
+            val stones = calculate25Blinks(stone, stonesMap25)
+            stonesMap25[stone] = stones
 
             for (stone2 in stones) {
-                val stones2 = calculate25Blinks(stone2, stonesMap)
-                stonesMap[stone2] = stones2
+                val stones2 = calculate25Blinks(stone2, stonesMap25)
+                stonesMap25[stone2] = stones2
 
                 for (stone3 in stones2) {
-                    val stones3 = calculate25Blinks(stone3, stonesMap)
-                    stonesMap[stone3] = stones3
+                    val stones3 = calculate25Blinks(stone3, stonesMap25)
+                    stonesMap25[stone3] = stones3
                     totalStones += stones3.size
                 }
             }
         }
-        println("total $totalStones")
         return totalStones
     }
 
-    private fun calculate25Blinks(stone: Long, stonesMap: MutableMap<Long, List<Long>>): List<Long> {
+    private fun calculate25Blinks(
+        stone: Long,
+        stonesMap: MutableMap<Long, List<Long>> = mutableMapOf(),
+    ): List<Long> {
         if (stonesMap[stone] != null) return stonesMap[stone]!!
         var stones = listOf(stone)
         repeat(25) {
@@ -58,16 +57,27 @@ class Day11(
         for (stone in stones) {
             when {
                 stone == 0L -> result.add(1)
-                stone.toString().length % 2 == 0 -> {
-                    val s = stone.toString()
-                    result.add(s.substring(0, s.length / 2).toLong())
-                    result.add(s.substring(s.length / 2).toLong())
+                stone.digits() % 2 == 0 -> {
+                    var p = 1L
+                    repeat(stone.digits() / 2) { p *= 10 }
+                    result.add(stone / p)
+                    result.add(stone % p)
                 }
                 else -> result.add(stone * 2024L)
             }
         }
         return result.toList()
     }
+}
+
+private fun Long.digits(): Int {
+    var p = 1
+    var n = this
+    while (n >= 10) {
+        n /= 10
+        p += 1
+    }
+    return p
 }
 
 fun main() {
