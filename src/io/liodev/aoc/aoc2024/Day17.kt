d44d31ca598ad4ba8a4900e00d583d5edaf00a66
LH23 <lionelhubmann@gmail.com>
2024-12-17 17:36:44 -0300

    Day17 implemented Computer, less params passing and only regs as Long
    Signed-off-by: LH23 <lionelhubmann@gmail.com>

--- src/io/liodev/aoc/aoc2024/Day17.kt
+++ src/io/liodev/aoc/aoc2024/Day17.kt
@@ -12,7 +12,7 @@ class Day17(
         listOf("5,7,3,0", "6,2,7,2,3,1,6,0,5", "117440", "236548287712877")
 
     private val initRegisters =
-        input.split("\n\n")[0].lines().map { it.substringAfter(": ").toInt() }
+        input.split("\n\n")[0].lines().map { it.substringAfter(": ").toLong() }
     private val program =
         input
             .split("\n\n")[1]
@@ -20,29 +20,90 @@ class Day17(
             .split(",")
             .map { it.toInt() }
 
-    override fun solvePart1(): String = runProgram(initRegisters.map { it.toLong() }).joinToString(",")
+    override fun solvePart1(): String = Computer(program).run(initRegisters).joinToString(",")
 
     override fun solvePart2(): String = findReverseRecursive("", program, 1)!!
 
-    private fun runProgram(registers: List<Long>): List<Int> {
-        val output = mutableListOf<Int>()
-        var regA = registers[0]
-        var regB = registers[1]
-        var regC = registers[2]
+    class Computer(
+        private val program: List<Int>,
+    ) {
+        private var ip = 0
+        private var regA = 0L
+        private var regB = 0L
+        private var regC = 0L
+        private val output = mutableListOf<Int>()
 
-        var halt = false
-        var ip = 0
-        while (!halt) {
-            val results = processInstruction(program[ip] to program[ip + 1], regA, regB, regC)
+        fun run(registers: List<Long>): List<Int> {
+            regA = registers[0]
+            regB = registers[1]
+            regC = registers[2]
 
-            regA = results[0]
-            regB = results[1]
-            regC = results[2]
-            if (results.size > 3 && results[3] != -1L) output.add(results[3].toInt())
-            ip = if (results.size > 4) results[4].toInt() else ip + 2
-            halt = ip >= program.size
+            var halt = false
+            ip = 0
+            while (!halt) {
+                processInstruction(program[ip] to program[ip + 1])
+                ip += 2
+                halt = ip >= program.size
+            }
+            return output
         }
-        return output
+
+        private fun processInstruction(instruction: Pair<Int, Int>) {
+            val (opcode, value) = instruction
+            when (opcode) {
+                0 -> adv(value)
+                1 -> bxl(value)
+                2 -> bst(value)
+                3 -> jnz(value)
+                4 -> bxc()
+                5 -> out(value)
+                6 -> bdv(value)
+                7 -> cdv(value)
+                else -> throw IllegalArgumentException("Unknown opcode: $opcode")
+            }
+        }
+
+        private fun adv(value: Int) {
+            regA = regA shr comboOperand(value).toInt()
+        }
+
+        private fun bxl(value: Int) {
+            regB = regB xor value.toLong()
+        }
+
+        private fun bst(value: Int) {
+            regB = comboOperand(value) % 8
+        }
+
+        private fun jnz(value: Int) {
+            if (regA != 0L) {
+                ip = value - 2
+            }
+        }
+
+        private fun bxc() {
+            regB = regB xor regC
+        }
+
+        private fun out(value: Int) {
+            output.add((comboOperand(value) % 8).toInt())
+        }
+
+        private fun bdv(value: Int) {
+            regB = regA shr comboOperand(value).toInt()
+        }
+
+        private fun cdv(value: Int) {
+            regC = regA shr comboOperand(value).toInt()
+        }
+
+        private fun comboOperand(value: Int): Long =
+            when (value) {
+                6 -> regC
+                5 -> regB
+                4 -> regA
+                else -> value.toLong()
+            }
     }
 
     private fun findReverseRecursive(
@@ -50,112 +111,18 @@ class Day17(
         program: List<Int>,
         n: Int,
     ): String? {
-        val bin3 = listOf("000", "001", "010", "011", "100", "101", "110", "111")
-        for (o in bin3) {
+        val bits = listOf("000", "001", "010", "011", "100", "101", "110", "111")
+        for (o in bits) {
             val newRegA = (result + o).toLong(2)
-            val output =
-                runProgram(listOf(newRegA, initRegisters[1].toLong(), initRegisters[2].toLong()))
+            val output = Computer(program).run(listOf(newRegA, initRegisters[1], initRegisters[2]))
             if (output == program) {
                 return "$newRegA"
             } else if (output.takeLast(n) == program.takeLast(n)) {
-                (findReverseRecursive(result + o, program, n + 1))?.let { return it }
+                (findReverseRecursive(result + o, program, n + 1))?.let { regA -> return regA }
             }
         }
         return null
     }
-
-    private fun processInstruction(
-        instruction: Pair<Int, Int>,
-        regA: Long,
-        regB: Long,
-        regC: Long,
-    ): List<Long> {
-        val (opcode, value) = instruction
-        return when (opcode) {
-            0 -> adv(value.toLong(), regA, regB, regC)
-            1 -> bxl(value.toLong(), regA, regB, regC)
-            2 -> bst(value.toLong(), regA, regB, regC)
-            3 -> jnz(value.toLong(), regA, regB, regC)
-            4 -> bxc(regA, regB, regC)
-            5 -> out(value.toLong(), regA, regB, regC)
-            6 -> bdv(value.toLong(), regA, regB, regC)
-            7 -> cdv(value.toLong(), regA, regB, regC)
-            else -> throw IllegalArgumentException("Unknown opcode: $opcode")
-        }
-    }
-
-    private fun adv(
-        value: Long,
-        regA: Long,
-        regB: Long,
-        regC: Long,
-    ): List<Long> = listOf(regA shr comboOperand(value, regA, regB, regC).toInt(), regB, regC)
-
-    private fun bxl(
-        value: Long,
-        regA: Long,
-        regB: Long,
-        regC: Long,
-    ): List<Long> = listOf(regA, regB xor value, regC)
-
-    private fun bst(
-        value: Long,
-        regA: Long,
-        regB: Long,
-        regC: Long,
-    ): List<Long> = listOf(regA, comboOperand(value, regA, regB, regC) % 8, regC)
-
-    private fun jnz(
-        value: Long,
-        regA: Long,
-        regB: Long,
-        regC: Long,
-    ): List<Long> =
-        if (regA == 0L) {
-            listOf(regA, regB, regC)
-        } else {
-            listOf(regA, regB, regC, -1, value)
-        }
-
-    private fun bxc(
-        regA: Long,
-        regB: Long,
-        regC: Long,
-    ): List<Long> = listOf(regA, regB xor regC, regC)
-
-    private fun out(
-        value: Long,
-        regA: Long,
-        regB: Long,
-        regC: Long,
-    ): List<Long> = listOf(regA, regB, regC, comboOperand(value, regA, regB, regC) % 8)
-
-    private fun bdv(
-        value: Long,
-        regA: Long,
-        regB: Long,
-        regC: Long,
-    ): List<Long> = listOf(regA, regA shr comboOperand(value, regA, regB, regC).toInt(), regC)
-
-    private fun cdv(
-        value: Long,
-        regA: Long,
-        regB: Long,
-        regC: Long,
-    ): List<Long> = listOf(regA, regB, regA shr comboOperand(value, regA, regB, regC).toInt())
-
-    private fun comboOperand(
-        value: Long,
-        regA: Long,
-        regB: Long,
-        regC: Long,
-    ): Long =
-        when (value) {
-            6L -> regC
-            5L -> regB
-            4L -> regA
-            else -> value
-        }
 }
 
 fun main() {
