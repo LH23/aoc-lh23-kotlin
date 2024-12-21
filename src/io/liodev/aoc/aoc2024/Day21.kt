5d3bd88b3bca6ab49353649f225358be5d16e645
LH23 <lionelhubmann@gmail.com>
2024-12-21 18:07:44 -0300

    shorter dpad keypad

--- src/io/liodev/aoc/aoc2024/Day21.kt
+++ src/io/liodev/aoc/aoc2024/Day21.kt
@@ -3,6 +3,7 @@ package io.liodev.aoc.aoc2024
 import io.liodev.aoc.Day
 import io.liodev.aoc.readInputAsString
 import io.liodev.aoc.runDay
+import io.liodev.aoc.utils.findFirstOrNull
 import kotlin.math.abs
 
 // --- 2024 Day 21: Keypad Conundrum ---
@@ -54,75 +55,61 @@ class Day21(
             }
         }
 
-    private fun rowKpad(c: Char): Int =
-        when (c) {
-            '7', '8', '9' -> 0
-            '4', '5', '6' -> 1
-            '1', '2', '3' -> 2
-            '0', 'A' -> 3
-            else -> throw IllegalArgumentException("Invalid KPAD character: $c")
-        }
-
-    private fun colKpad(c: Char) =
-        when (c) {
-            '7', '4', '1' -> 0
-            '8', '5', '2', '0' -> 1
-            '9', '6', '3', 'A' -> 2
-            else -> throw IllegalArgumentException("Invalid KPAD character: $c")
-        }
+    private val keypad =
+        listOf(
+            listOf('7', '8', '9'),
+            listOf('4', '5', '6'),
+            listOf('1', '2', '3'),
+            listOf(' ', '0', 'A'),
+        )
 
     private fun movesKeypad(
-        currentPos: Char,
+        current: Char,
         dest: Char,
     ): List<String> {
-        return if (currentPos == dest) {
+        return if (current == dest) {
             listOf("A")
         } else {
-            val diffCol = colKpad(currentPos) - colKpad(dest)
+            val currCoord = keypad.findFirstOrNull(current)!!
+            val destCoord = keypad.findFirstOrNull(dest)!!
+            val diffCol = currCoord.c - destCoord.c
             val horizontal = (if (diffCol < 0) ">" else "<").repeat(abs(diffCol))
-            val diffRow = rowKpad(currentPos) - rowKpad(dest)
+            val diffRow = currCoord.r - destCoord.r
             val vertical = (if (diffRow < 0) "v" else "^").repeat(abs(diffRow))
 
             return when {
-                rowKpad(currentPos) == 3 && colKpad(dest) == 0 -> listOf(vertical + horizontal + "A")
-                colKpad(currentPos) == 0 && rowKpad(dest) == 3 -> listOf(horizontal + vertical + "A")
-                colKpad(currentPos) == colKpad(dest) -> listOf(vertical + "A")
+                currCoord.r == 3 && destCoord.c == 0 -> listOf(vertical + horizontal + "A")
+                currCoord.c == 0 && destCoord.r == 3 -> listOf(horizontal + vertical + "A")
+                currCoord.c == destCoord.c -> listOf(vertical + "A")
                 vertical.isEmpty() || horizontal.isEmpty() -> listOf(vertical + horizontal + "A")
                 else -> listOf(horizontal + vertical + "A", vertical + horizontal + "A")
             }
         }
     }
 
-    private fun rowDpad(c: Char): Int =
-        when (c) {
-            '^', 'A' -> 0
-            '<', 'v', '>' -> 1
-            else -> throw IllegalArgumentException("Invalid DPAD character: $c")
-        }
-
-    private fun colDpad(c: Char) =
-        when (c) {
-            '<' -> 0
-            'v', '^' -> 1
-            '>', 'A' -> 2
-            else -> throw IllegalArgumentException("Invalid DPAD character: $c")
-        }
+    private val dpad =
+        listOf(
+            listOf(' ', '^', 'A'),
+            listOf('<', 'v', '>'),
+        )
 
     private fun movesDpad(
-        currentPos: Char,
+        current: Char,
         dest: Char,
     ): List<String> {
-        return if (currentPos == dest) {
+        return if (current == dest) {
             listOf("A")
         } else {
-            val diffCol = colDpad(currentPos) - colDpad(dest)
+            val currCoord = dpad.findFirstOrNull(current)!!
+            val destCoord = dpad.findFirstOrNull(dest)!!
+            val diffCol = currCoord.c - destCoord.c
             val horizontal = (if (diffCol < 0) ">" else "<").repeat(abs(diffCol))
-            val diffRow = rowDpad(currentPos) - rowDpad(dest)
+            val diffRow = currCoord.r - destCoord.r
             val vertical = (if (diffRow < 0) "v" else "^").repeat(abs(diffRow))
 
             return when {
-                colDpad(currentPos) == 0 && rowDpad(dest) == 0 -> listOf(horizontal + vertical + "A")
-                rowDpad(currentPos) == 0 && colDpad(dest) == 0 -> listOf(vertical + horizontal + "A")
+                currCoord.c == 0 && destCoord.r == 0 -> listOf(horizontal + vertical + "A")
+                currCoord.r == 0 && destCoord.c == 0 -> listOf(vertical + horizontal + "A")
                 vertical.isEmpty() || horizontal.isEmpty() -> listOf(vertical + horizontal + "A")
                 else -> listOf(horizontal + vertical + "A", vertical + horizontal + "A")
             }
