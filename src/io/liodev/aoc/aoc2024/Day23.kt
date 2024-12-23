b26e36823de5ce9aa040b1fb57a118edde176389
LH23 <lionelhubmann@gmail.com>
2024-12-23 03:30:34 -0300

    Day23 cleanup

--- src/io/liodev/aoc/aoc2024/Day23.kt
+++ src/io/liodev/aoc/aoc2024/Day23.kt
@@ -10,9 +10,9 @@ class Day23(
 ) : Day<String> {
     override val expectedValues = listOf("7", "1368", "co,de,ka,ta", "dd,ig,il,im,kb,kr,pe,ti,tv,vr,we,xu,zi")
 
-    private val networkPairs = input.lines().map { line -> line.split("-").let { it[0] to it[1] } }
     private val networkGraph =
         buildMap<String, MutableSet<String>> {
+            val networkPairs = input.lines().map { line -> line.split("-").let { it[0] to it[1] } }
             networkPairs.forEach { (a, b) ->
                 getOrPut(a) { mutableSetOf() }.add(b)
                 getOrPut(b) { mutableSetOf() }.add(a)
@@ -34,36 +34,31 @@ class Day23(
         return lan3.size.toString()
     }
 
-    override fun solvePart2(): String = findBiggestConnectedSet().sorted().joinToString(",")
-
-    private fun findBiggestConnectedSet(): Set<String> = networkGraph.largestClique()
-
+    override fun solvePart2(): String = networkGraph.largestClique().sorted().joinToString(",")
 }
 
-private fun Map<String, Set<String>>.largestClique(): Set<String> {
-    return this.runBronKerbosch(this.keys)
-}
+private fun Map<String, Set<String>>.largestClique(): Set<String> = this.runBronKerbosch(this.keys)
 
-//algorithm BronKerbosch1(R, P, X) is
+//algorithm BronKerbosch2(R, P, X) is
 //    if P and X are both empty then
 //        report R as a maximal clique
-//    for each vertex v in P do
-//        BronKerbosch1(R ⋃ {v}, P ⋂ N(v), X ⋂ N(v))
+//    choose a pivot vertex u in P ⋃ X (* pivot with most neighbors minimize recursive calls)
+//    for each vertex v in P \ N(u) do
+//        BronKerbosch2(R ⋃ {v}, P ⋂ N(v), X ⋂ N(v))
 //        P := P \ {v}
 //        X := X ⋃ {v}
 fun Map<String, Set<String>>.runBronKerbosch(
     p: Set<String>,
     r: Set<String> = emptySet(),
     x: Set<String> = emptySet(),
-    candidates: MutableSet<Set<String>> = mutableSetOf()
+    candidates: MutableSet<Set<String>> = mutableSetOf(),
 ): Set<String> {
     if (p.isEmpty() && x.isEmpty() && r !in candidates) {
-        if (r.size > (candidates.maxOfOrNull { it.size } ?: 0)) {
-            candidates.add(r)
-        }
+        candidates.add(r)
     } else {
         val mostNeighbors = (p + x).maxByOrNull { this[it]!!.size }
-        for (v in if (mostNeighbors != null) p.minus(this[mostNeighbors]!!) else p) {
+        val p2 = if (mostNeighbors != null) p.minus(this[mostNeighbors]!!) else p
+        for (v in p2) {
             runBronKerbosch(p.intersect(this[v]!!), r + v, x.intersect(this[v]!!), candidates)
         }
     }
@@ -75,5 +70,5 @@ fun main() {
     val year = 2024
     val testInput = readInputAsString("src/input/$year/${name}_test.txt")
     val realInput = readInputAsString("src/input/$year/$name.txt")
-    runDay(Day23(testInput), Day23(realInput), year, printTimings = true)
+    runDay(Day23(testInput), Day23(realInput), year)
 }
