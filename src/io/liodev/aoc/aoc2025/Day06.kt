90c005ab86e2ba23b82b330d1548eef668632318
LH23 <lionelhubmann@gmail.com>
2025-12-06 03:48:41 -0300

    Day06: initial version

---
+++ src/io/liodev/aoc/aoc2025/Day06.kt
@@ -0,0 +1,82 @@
+package io.liodev.aoc.aoc2025
+
+import io.liodev.aoc.Day
+import io.liodev.aoc.readInputAsString
+import io.liodev.aoc.runDay
+
+// --- 2025 Day 6: Trash Compactor ---
+class Day06(
+    input: String,
+) : Day<Long> {
+    override val expectedValues = listOf(4277556L, 7098065460541, 3263827, 13807151830618)
+
+    private val operationsListStr = input.split("\n")
+
+    private data class Operation(
+        val operands: List<Long>,
+        val operation: String,
+    ) {
+        fun solve(): Long {
+            return when (operation) {
+                "+" -> operands.sum()
+                "*" -> operands.fold(1L) { acc, l -> acc * l }
+                else -> 0L
+            }
+        }
+    }
+
+    override fun solvePart1(): Long {
+        val operationsList = operationsListStr.map { line -> line.split("\\s".toRegex()).filter { it.isNotEmpty() } }
+        val operandsIndices = 0..<operationsList.lastIndex
+        val operationIndex = operationsList.lastIndex
+
+        return operationsList[0].indices.map { j ->
+            Operation(
+                operands = operandsIndices.map { i -> operationsList[i][j].toLong() },
+                operation = operationsList[operationIndex][j],
+            )
+        }.sumOf { it.solve() }
+    }
+
+    override fun solvePart2(): Long {
+        val operationIndex = operationsListStr.lastIndex
+        val operationPositions = operationsListStr[operationIndex].withIndex().filter { (_, value) -> value != ' ' }.map { it.index }
+        val chunkSizes = operationPositions.zipWithNext().map { (a,b) -> b - a - 1} + (operationsListStr.maxOf{ it.length } - operationPositions.last())
+
+        val operationsList = operationsListStr.map { line ->
+            buildList {
+                var currentIndex = 0
+                for (size in chunkSizes) {
+                    val chunk = if (currentIndex + size > line.length)
+                        line.substring(currentIndex).padEnd(size, ' ')
+                    else
+                        line.substring(currentIndex, currentIndex + size)
+                    add(chunk)
+                    currentIndex += size + 1
+                }
+            }
+        }
+        val operandsIndices = 0..<operationsList.lastIndex
+
+        return operationsList[0].indices.map { j ->
+            Operation(
+                operands = operandsIndices.map { i -> operationsList[i][j].toList() }.transpose().map { it.joinToString("").trim().toLong() },
+                operation = operationsList[operationIndex][j].trim(),
+            )
+        }.sumOf { it.solve() }
+    }
+}
+
+private fun List<List<Char>>.transpose(): List<List<Char>> {
+    return this[0].indices.map { j ->
+        this.indices.map { i -> this[i][j] }
+    }
+}
+
+fun main() {
+    val name = Day06::class.simpleName
+    val year = 2025
+    val testInput = readInputAsString("src/input/$year/${name}_test.txt")
+    val realInput = readInputAsString("src/input/$year/$name.txt")
+    runDay(Day06(testInput), Day06(realInput), year)
+}
