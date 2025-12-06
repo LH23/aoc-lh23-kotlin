7598e10f1943315c479a9a84ad259c488fc94261
LH23 <lionelhubmann@gmail.com>
2025-12-06 18:58:33 -0300

    Day06: streamline part1/part2 methods

--- src/io/liodev/aoc/aoc2025/Day06.kt
+++ src/io/liodev/aoc/aoc2025/Day06.kt
@@ -10,7 +10,7 @@ class Day06(
 ) : Day<Long> {
     override val expectedValues = listOf(4277556L, 7098065460541, 3263827, 13807151830618)
 
-    private val operationsListStr = input.split("\n")
+    private val operationsList = input.split("\n").toOperationsList()
 
     private data class Operation(
         val operands: List<Long>,
@@ -25,51 +25,44 @@ class Day06(
         }
     }
 
-    override fun solvePart1(): Long {
-        val operationsList =
-            operationsListStr.map { line -> line.split("\\s+".toRegex()).filter { it.isNotEmpty() } }
-        val operationsIndices = operationsList[0].indices
-        val operandsIndices = 0..<operationsList.lastIndex
-        val operationIndex = operationsList.lastIndex
+    override fun solvePart1(): Long = solveHomework { charArray -> charArray.asLongs() } 
 
-        return operationsIndices.map { j ->
+    override fun solvePart2(): Long = solveHomework { charArray -> charArray.transpose().asLongs() }
+
+    fun solveHomework(getOperands: (List<List<Char>>) -> List<Long>): Long {
+        return operationsList[0].indices.map { j ->
+            val x = (0..<operationsList.lastIndex).map { i -> operationsList[i][j].toList() }
             Operation(
-                operands = operandsIndices.map { i -> operationsList[i][j].toLong() },
-                operation = operationsList[operationIndex][j],
-            )
-        }.sumOf { it.solve() }
-    }
-
-    override fun solvePart2(): Long {
-        val operationIndex = operationsListStr.lastIndex
-        val maxLineSize = operationsListStr.maxOf { it.length }
-        val operationPositions =
-            operationsListStr[operationIndex].withIndex().filter { (_, value) -> value != ' ' }.map { it.index }
-        val chunkSizes = 
-            operationPositions.zipWithNext().map { (a, b) -> b - a - 1 } + (maxLineSize - operationPositions.last())
-
-        val operationsList = operationsListStr.map { line ->
-            //val paddedLine = line.padEnd(maxLineSize, ' ') <- AndroidStudio removing my trailing whitespaces! -_-
-            buildList {
-                var currentIndex = 0
-                for (size in chunkSizes) {
-                    add(line.substring(currentIndex, currentIndex + size))
-                    currentIndex += size + 1
-                }
-            }
-        }
-        val operationsIndices = operationsList[0].indices
-        val operandsIndices = 0..<operationsList.lastIndex
-
-        return operationsIndices.map { j ->
-            Operation(
-                operands = operandsIndices.map { i -> operationsList[i][j].toList() }.transpose()
-                    .map { it.joinToString("").trim().toLong() },
-                operation = operationsList[operationIndex][j].trim(),
+                operands = getOperands(x),
+                operation = operationsList[operationsList.lastIndex][j].trim(),
             )
         }.sumOf { it.solve() }
     }
 }
+
+private fun List<String>.toOperationsList(): List<List<String>> {
+    val lines = this
+    val maxLineSize = lines.maxOf { it.length } // <- AndroidStudio removing trailing whitespaces! -_-
+
+    val emptyColumnsIndices = listOf(-1) +
+            lines[0].indices.filter {
+                    j -> lines.indices.all { i -> lines[i][j] == ' ' }
+            } + listOf(maxLineSize)
+    val chunkSizes = emptyColumnsIndices.zipWithNext().map { (a,b) -> b - a - 1 }
+
+    return lines.map { line ->
+        val paddedLine = line.padEnd(maxLineSize, ' ') // <- AndroidStudio removing trailing whitespaces! -_-
+        buildList {
+            var currentIndex = 0
+            for (size in chunkSizes) {
+                add(paddedLine.substring(currentIndex, currentIndex + size))
+                currentIndex += size + 1
+            }
+        }
+    }
+}
+
+private fun List<List<Char>>.asLongs(): List<Long> = this.map { it.joinToString("").trim().toLong() }
 
 private fun List<List<Char>>.transpose(): List<List<Char>> {
     return this[0].indices.map { j ->
